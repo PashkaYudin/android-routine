745caa05fb248b4258ce5f3892b4d097c7aa9a21
PashkaYudin <coolbatmanarrow@gmail.com>
2016-07-05 18:30:16 +0600

    web page commit

---
+++ javascripts/main.js
@@ -0,0 +1,71 @@
+﻿$(document).ready(function() {
+    $(document).on("click", "#report", function() {
+            getResult();
+            return false;
+    });
+
+	$(document).on("click", "#add", function() {
+        var table = $("table"),
+            body = table.find("tbody"),
+            thisTR = table.find("tr").last(),
+            newTR = thisTR.clone();
+			newTR.find('input').val("");
+			body.append(newTR);
+			thisTR.find("input").each(function() {
+				$(this).parent().html($(this).val() == "" ? "0" : $(this).val());
+			});
+			thisTR.find("i").hide();
+			return false;
+    });
+});
+
+
+    function getResult() {
+        var ClassName = $('.name').val();
+        var count = $('#data tbody tr').length;
+        var arrVars = [];
+        var arrTypes = [];
+        $("#data tr").each(function(){
+            arrVars.push($(this).find(".variable").text()); //put elements into array
+            arrTypes.push($(this).find(".type").val()); //put elements into array
+        });
+
+        var result = 'public class ' + ClassName + ' {'+ '\n';
+
+        for (var i = 1; i <= count-1; i++) {
+            result += '\t'+'private '+ arrTypes[i] +' '+ arrVars[i] +';'+'\n';
+        }
+
+        result += '\n\t'+'public  '+ ClassName +'() {'+'\n'+
+                  '\t'+'}'+'\n\n';
+
+        result += '\t'+'public '+ ClassName +'(';
+
+        for (var i = 1; i <= count-1; i++) {
+            result += arrTypes[i] +' '+ arrVars[i] +', ';
+        }
+        result = result.slice(0,-2);
+        result += ') {'+'\n';
+
+        for (var i = 1; i <= count-1; i++) {
+            result += '\t\t'+'this.'+arrVars[i]+' = '+arrVars[i]+'\n';
+        }
+        result += '\t'+'}'+'\n\n';
+
+        for (var i = 1; i <= count-1; i++) {
+        result += '\t'+'public '+ arrTypes[i] +' get'+ arrVars[i].capitalize() +'() {'+'\n'+
+                    '\t\t'+'return '+ arrVars[i] +';'+'\n'+
+                    '\t'+'}'+'\n\n'+
+                    '\t'+'public void set'+ arrVars[i].capitalize() +'('+ arrTypes[i] +' '+ arrVars[i] +') {'+'\n'+
+                        '\t\t'+'this.'+ arrVars[i] +' = '+ arrVars[i] +';'+'\n'+
+                    '\t'+'}'+'\n\n';                    
+        }  
+        result = result.slice(0,-2);
+        result += '\n'+'}';             
+                     
+        $('.result').append('<pre><code class="language-java">' + result + '</code></pre>');
+    }
+
+    String.prototype.capitalize = function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    }
